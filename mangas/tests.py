ad978aa3d00bb8cd28eb10d945af5e1c7237ecee
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-04-23 15:13:48 -0600

    Cambios

--- mangas/tests.py
+++ mangas/tests.py
@@ -3,7 +3,6 @@ from mixer.backend.django import mixer
 import graphene
 import json
 
-
 # Create your tests here.
 from mangas.schema import schema
 from mangas.models import Manga
@@ -31,6 +30,15 @@ CREATE_MANGA_MUTATION = '''
 mutation createMangaMutation($titulomanga: String, $generomanga: String, $descripcionmanga: String, $lanzamientomanga: String, $escritormanga: String, $volumenesmanga: Int, $capitulosmanga: Int, $ilustradormanga: String, $editorialmanga: String, $clasificacionedadmanga: Int ){
     createManga(titulomanga: $titulomanga, generomanga: $generomanga, descripcionmanga: $descripcionmanga,  lanzamientomanga: $lanzamientomanga,  escritormanga: $escritormanga,  volumenesmanga: $volumenesmanga,  capitulosmanga: $capitulosmanga,  ilustradormanga: $ilustradormanga,  editorialmanga: $editorialmanga, clasificacionedadmanga: $clasificacionedadmanga){
     titulomanga
+        generomanga
+        descripcionmanga
+        lanzamientomanga
+        escritormanga
+        volumenesmanga
+        capitulosmanga
+        ilustradormanga
+        editorialmanga
+        clasificacionedadmanga
  }
 }
 '''
@@ -40,6 +48,17 @@ class MangaTestCase(GraphQLTestCase) :
         
     def setUp(self):
         self.manga1 = mixer.blend(Manga)
+        self.manga2 = mixer.blend(Manga)
+        
+    def test_mangas_query(self):
+        response = self.query(
+            LINKS_QUERY,
+        )
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        print ("query mangas results")
+        print (content)
+        assert len(content['data']['mangas'])==2
         
     def test_createLink_mutation(self):
         response = self.query(
@@ -51,15 +70,7 @@ class MangaTestCase(GraphQLTestCase) :
         content = json.loads(response.content)
         print(content)
         self.assertResponseNoErrors(response)
-        self.assertDictEqual({"createManga": {"titulomanga": "Kimi no Todoke!"}}, content['data'])
-        
-    def test_mangas_query(self):
-        response = self.query(
-            LINKS_QUERY,
-            )
-        content = json.loads(response.content)
-        self.assertResponseNoErrors(response)
-        print ("query mangas results")
-        print (content)
-        assert len(content['data']['mangas'])==1
+        self.assertDictEqual({"createManga": {"titulomanga": "Kimi no Todoke!",  'generomanga': 'Romance', 'descripcionmanga': 'Joder claro que si', 'lanzamientomanga': '2002-04-24', 'escritormanga': 'Dios', 'volumenesmanga': 2, 'capitulosmanga': 5, 'ilustradormanga': 'Yo', 'editorialmanga': 'El cielo', 'clasificacionedadmanga': 12 }}, content['data'])
+                
+   
 
